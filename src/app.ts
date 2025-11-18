8a6752563f84a80fbf5d7411f524c099364620bc
sudowizards <iba011@proton.me>
2025-11-18 14:31:20 +0100

    add games requests

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import {games} from "./data/gamesData";
+import {Game} from "./models/game";
 const app = express();
 //..
 
@@ -16,3 +17,35 @@ app.get("/house", (req, res) => {
 app.get("/garden", (req, res) => {
     res.json(games)
 });
+
+app.get("/games", (req: express.Request, res: express.Response) => {
+    const {year, platform} = req.query
+
+    if(!year || !platform) res.json(games)
+    const filteredGames = games.filter(g => {
+        if(year && g.year > Number(year)) return false
+
+        if(platform) {
+            if(typeof platform === "string") {
+                const queryPlatform = platform.toLowerCase()
+                const gamePlatform = g.platform.toLowerCase()
+                if(!gamePlatform.includes(queryPlatform)) return false
+            }
+        }
+        return false
+        })
+        res.json(filteredGames)
+    }
+)
+
+// route parameter
+app.get("games/:id", (req: express.Request, res: express.Response) => {
+    const game = games.find(game => game.id === Number(req.params.id))
+    if(!game) return res.status(404).send("No game found.")
+    res.json(game)
+})
+
+app.post("/games", (req: express.Request, res: express.Response) =>
+    console.log(req.body)
+    // TODO
+)
